4656b108c6bfb28fa2952f4ba80613f9e947aa7a
Fabien Houang <houang.fabien@gmail.com>
2025-05-03 12:56:12 -0400

    Fix: Add npm deploy script for Render

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ set -o errexit
 
 mix deps.get --only prod
 MIX_ENV=prod mix compile
+MIX_ENV=prod mix phx.digest
 
 cd assets && npm install && npm run deploy
 cd ..
 
-MIX_ENV=prod mix phx.digest
+
